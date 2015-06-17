866d7806e9f8df5332e1030c9ea4e9d619d614ae
Alexandre Leray <alexandre@stdin.fr>
2015-06-17 14:00:36 +0200

    Remove tastypie version for now. I will try to force installing django 1.8 compatible tastypie version from requirements instead, like this:
       --allow-external django-tastypie
       --allow-unverified django-tastypie
    
       git+git://github.com/django-tastypie/django-tastypie@256ebe1de9a78dfb5d4d6e938b813cf4c5c4ac1b

--- setup.py
+++ setup.py
@@ -17,12 +17,9 @@ setup(
     ],
     include_package_data = True,
     install_requires=[
-        'django-tastypie==0.12.2-dev',
+        'django-tastypie',
         'django-guardian==1.3',
         'GitPython==0.1.7'
-    ],
-    dependency_links=[
-        'https://github.com/django-tastypie/django-tastypie/archive/256ebe1de9a78dfb5d4d6e938b813cf4c5c4ac1b.zip#egg=django-tastypie-0.12.2-dev'
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
