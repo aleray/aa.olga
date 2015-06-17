691b9f7179543a3c6704ab909e0fb4f918f48e84
Alexandre Leray <alexandre@stdin.fr>
2015-06-17 13:01:45 +0200

    Fixed dango tastypie dependency

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ setup(
     ],
     include_package_data = True,
     install_requires=[
-        'django-tastypie',
+        'django-tastypie==0.12.2-dev',
         'django-guardian==1.3',
         'GitPython==0.1.7'
     ],
     dependency_links=[
-        'https://github.com/django-tastypie/django-tastypie/archive/master.zip#egg=django-tastypie'
+        'https://github.com/django-tastypie/django-tastypie/archive/256ebe1de9a78dfb5d4d6e938b813cf4c5c4ac1b.zip#egg=django-tastypie-0.12.2-dev'
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
